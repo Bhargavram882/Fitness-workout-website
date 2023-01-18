c79a452241ead1b5b131009aa108002d5d397aa2
Bhargavram882 <bhargavram5882@gmail.com>
2023-01-18 11:10:26 +0530

    second

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/images/372.jpg';
+
+const Footer = () => (
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+      <img src={Logo} alt="logo" style={{ width: '100px', height: '81px' }} />
+    </Stack>
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Contact Us for membership!</Typography>
+  </Box>
+);
+
+export default Footer;
