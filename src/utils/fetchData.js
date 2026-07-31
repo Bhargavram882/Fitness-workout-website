7ed4987fad0d959ee0e0ed26d9ae1a6091fd3a6a
BHARGAVRAM GURRAM <73090813+Bhargavram882@users.noreply.github.com>
2026-07-31 13:30:14 -0400

    fix: add getExerciseGifUrl helper for the new authenticated image endpoint
    exercisedb.p.rapidapi.com no longer includes gifUrl on exercise
    objects; images now require GET /image?exerciseId=X with the API key
    passed as a query param (img tags can't send custom headers).

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,8 +1,10 @@
+const RAPIDAPI_KEY = '4621deb6a9msh7a4489e3de96da3p1f5098jsn7fb616c05a6a';
+
 export const exerciseOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': '4621deb6a9msh7a4489e3de96da3p1f5098jsn7fb616c05a6a',
+    'X-RapidAPI-Key': RAPIDAPI_KEY,
   },
 };
 
@@ -13,6 +15,12 @@ export const youtubeOptions = {
     'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
   },
 };
+
+// The API no longer returns a gifUrl on exercise objects; images now live
+// behind an authenticated endpoint that requires the key as a query param
+// since <img> tags can't send custom headers.
+export const getExerciseGifUrl = (id, resolution = 180) =>
+  `https://exercisedb.p.rapidapi.com/image?exerciseId=${id}&resolution=${resolution}&rapidapi-key=${RAPIDAPI_KEY}`;
 
 export const fetchData = async (url, options) => {
   const res = await fetch(url, options);
