49a5112446476aebcab8a9100f49461ff85fa777
BHARGAVRAM GURRAM <73090813+Bhargavram882@users.noreply.github.com>
2026-07-31 13:30:14 -0400

    fix: use getExerciseGifUrl instead of missing exercise.gifUrl

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Stack, Typography } from '@mui/material';
 
+import { getExerciseGifUrl } from '../utils/fetchData';
+
 const ExerciseCard = ({ exercise }) => (
   <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
-    <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+    <img src={getExerciseGifUrl(exercise.id)} alt={exercise.name} loading="lazy" />
     <Stack direction="row">
       <Button sx={{ ml: '21px', color: '#fff', background: '#FFA9A9', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
         {exercise.bodyPart}
